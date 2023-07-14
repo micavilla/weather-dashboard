ef42e24049b3a0c670e68f1b502a61722d7f87bf
micavilla <micaella.villa1@gmail.com>
2023-07-14 01:13:53 -0500

    added js rules to display weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,8 +27,6 @@ function cityHistory() {
   }
 }
 
-cityHistory();
-
 function getCity() {
   var inputCity = document.getElementById('search-bar').value;
   //console.log(userInput);
@@ -46,8 +44,11 @@ function getCity() {
       var lon =(data.coord.lon);
       console.log(lat,lon);
       getCoor(lat, lon);
+      getFuture(lat, lon);
   })
 };
+
+var currentDay = document.getElementById('currentDay');
 
 function getCoor(lat, lon) {
   var coordinateAPI = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=dc51e86f28b41d77ff8619417bc35d08";
@@ -58,11 +59,201 @@ function getCoor(lat, lon) {
     })
     .then(function (data) {
       console.log(data);
+      currentDay.innerHTML = "";
       var temp = (data.list[0].main.temp);
+      var faren = (temp-273.15)*(9/5)+32;
       var humidity = (data.list[0].main.humidity);
       var wind = (data.list[0].wind.speed);
       var date = (data.list[0].dt_txt);
+      var formatDate = dayjs(date).format('M/D/YYYY');
       var place = (data.city.name);
       var weather = (data.list[0].weather[0].main);
+
+      var leCity = document.getElementById('leCity');
+      leCity.innerText = place + " " + formatDate;
+
+      var leTemp = document.createElement("li");
+      leTemp.textContent = "Temp: " + Math.floor(faren) + " F";
+
+      var leWind = document.createElement("li");
+      leWind.textContent = "Wind: " + wind + " MPH";
+
+      var leHumidity = document.createElement("li");
+      leHumidity.textContent = "Humidity: " + humidity + "%";
+
+      currentDay.appendChild(leTemp);
+      currentDay.appendChild(leWind);
+      currentDay.appendChild(leHumidity);
     })
 };
+
+function getFuture(lat, lon) {
+  var coordinateAPI = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=dc51e86f28b41d77ff8619417bc35d08";
+
+  fetch(coordinateAPI)
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (data){
+    console.log(data);
+    var day1 = document.getElementById('day1-list');
+    day1.innerHTML = "";
+
+    var date = (data.list[8].dt_txt);
+    var formatDate = dayjs(date).format('M/D/YYYY');
+    var temp = (data.list[8].main.temp);
+    var faren = (temp-273.15)*(9/5)+32;
+    var wind = (data.list[8].wind.speed);
+    var humidity = (data.list[8].main.humidity);
+    
+    var date1 = document.getElementById('day1');
+    date1.innerText = formatDate;
+
+    var leTemp = document.createElement('li');
+    leTemp.textContent = "Temp: " + Math.floor(faren) + " F";
+
+    var leWind = document.createElement("li");
+    leWind.textContent = "Wind: " + wind + " MPH";
+
+    var leHumidity = document.createElement("li");
+    leHumidity.textContent = "Humidity: " + humidity + "%";
+
+    day1.appendChild(leTemp);
+    day1.appendChild(leWind);
+    day1.appendChild(leHumidity);
+})
+
+  fetch(coordinateAPI)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data){
+    console.log(data);
+    var day2 = document.getElementById('day2-list');
+    day2.innerHTML = "";
+
+    var date = (data.list[16].dt_txt);
+    var formatDate = dayjs(date).format('M/D/YYYY');
+    var temp = (data.list[16].main.temp);
+    var faren = (temp-273.15)*(9/5)+32;
+    var wind = (data.list[16].wind.speed);
+    var humidity = (data.list[16].main.humidity);
+  
+    var date2 = document.getElementById('day2');
+    date2.innerText = formatDate;
+
+    var leTemp = document.createElement('li');
+    leTemp.textContent = "Temp: " + Math.floor(faren) + " F";
+
+    var leWind = document.createElement("li");
+    leWind.textContent = "Wind: " + wind + " MPH";
+
+    var leHumidity = document.createElement("li");
+    leHumidity.textContent = "Humidity: " + humidity + "%";
+
+    day2.appendChild(leTemp);
+    day2.appendChild(leWind);
+    day2.appendChild(leHumidity);
+})
+
+fetch(coordinateAPI)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data){
+  console.log(data);
+  var day3 = document.getElementById('day3-list');
+  day3.innerHTML = "";
+
+  var date = (data.list[24].dt_txt);
+  var formatDate = dayjs(date).format('M/D/YYYY');
+  var temp = (data.list[24].main.temp);
+  var faren = (temp-273.15)*(9/5)+32;
+  var wind = (data.list[24].wind.speed);
+  var humidity = (data.list[24].main.humidity);
+  
+  var date1 = document.getElementById('day3');
+  date1.innerText = formatDate;
+
+  var leTemp = document.createElement('li');
+  leTemp.textContent = "Temp: " + Math.floor(faren) + " F";
+
+  var leWind = document.createElement("li");
+  leWind.textContent = "Wind: " + wind + " MPH";
+
+  var leHumidity = document.createElement("li");
+  leHumidity.textContent = "Humidity: " + humidity + "%";
+
+  day3.appendChild(leTemp);
+  day3.appendChild(leWind);
+  day3.appendChild(leHumidity);
+})
+
+fetch(coordinateAPI)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data){
+  console.log(data);
+  var day4 = document.getElementById('day4-list');
+  day4.innerHTML = "";
+
+  var date = (data.list[32].dt_txt);
+  var formatDate = dayjs(date).format('M/D/YYYY');
+  var temp = (data.list[32].main.temp);
+  var faren = (temp-273.15)*(9/5)+32;
+  var wind = (data.list[32].wind.speed);
+  var humidity = (data.list[32].main.humidity);
+  
+  var date1 = document.getElementById('day4');
+  date1.innerText = formatDate;
+
+  var leTemp = document.createElement('li');
+  leTemp.textContent = "Temp: " + Math.floor(faren) + " F";
+
+  var leWind = document.createElement("li");
+  leWind.textContent = "Wind: " + wind + " MPH";
+
+  var leHumidity = document.createElement("li");
+  leHumidity.textContent = "Humidity: " + humidity + "%";
+
+  day4.appendChild(leTemp);
+  day4.appendChild(leWind);
+  day4.appendChild(leHumidity);
+})
+
+fetch(coordinateAPI)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data){
+  console.log(data);
+  var day5 = document.getElementById('day5-list');
+  day5.innerHTML = "";
+
+  var date = (data.list[39].dt_txt);
+  var formatDate = dayjs(date).format('M/D/YYYY');
+  var temp = (data.list[39].main.temp);
+  var faren = (temp-273.15)*(9/5)+32;
+  var wind = (data.list[39].wind.speed);
+  var humidity = (data.list[39].main.humidity);
+  
+  var date5 = document.getElementById('day5');
+  date5.innerText = formatDate;
+
+  var leTemp = document.createElement('li');
+  leTemp.textContent = "Temp: " + Math.floor(faren) + " F";
+
+  var leWind = document.createElement("li");
+  leWind.textContent = "Wind: " + wind + " MPH";
+
+  var leHumidity = document.createElement("li");
+  leHumidity.textContent = "Humidity: " + humidity + "%";
+
+  day5.appendChild(leTemp);
+  day5.appendChild(leWind);
+  day5.appendChild(leHumidity);
+})
+}
+
+cityHistory();
